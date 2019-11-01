012357dfb858c80caaec0878c7e345fc9deb29c4
DaAyupov21 <adamir21198@gmail.com>
2019-11-01 22:36:06 +0300

    finish

--- Svertka.cpp
+++ Svertka.cpp
@@ -4,31 +4,25 @@
 
 #include "Svertka.h"
 #include <iomanip>
-#include "stdio.h"
+
 Svertka::Svertka(vector<vector<int>> m, vector<vector<int>> k) {
-    int res = 0;
     matrix = m;
     kernel = k;
     result.resize(matrix.size()-kernel.size()+1);
     for(auto & i : result){
         i.resize(matrix.size()-kernel.size() + 1, 0); //создаем размерность матрицы после свертки
     }
-    PrintResult();
-    cout << "\n\n";
-    PrintMatrix();
 cout << endl;
     for(int i = 0; i < result.size(); i++){
         for(int j = 0; j < result[i].size(); j++){
-            res = 0;
             for(int ik = 0; ik < kernel.size(); ik++) {
-                for (int jk = 0; jk < kernel[i].size(); jk++) {
-                    if(i + ik > matrix.size() || j + jk > matrix[i].size()){
+                for (int jk = 0; jk < kernel[ik].size(); jk++) {
+                    if (i + ik > matrix.size() || j + jk > matrix[i].size()) {
                         continue;
                     }
                     result[i][j] += matrix[i + ik][j + jk] * kernel[ik][jk];
                 }
             }
-            cout<<setw(5) << result[i][j];
         }
         cout << endl;
     }
@@ -37,7 +31,6 @@ cout << endl;
 void Svertka::PrintResult() {
     for(int i = 0; i < result.size(); i++){
         for (int j = 0; j < result[i].size(); j++){
-
             cout << setw(5) << result[i][j];
         }
         cout << endl;
